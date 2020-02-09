cbed8fc1ece57bc9ed647be29a1675414e7e544d
timjolson <timjolson@users.noreply.github.com>
2020-02-09 16:07:55 -0600

    removed email

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ setup(
     name='mp_utils',
     version="0.5",
     description='python3 utils for multiprocessing',
-    author = 'Tim Olson',
-    author_email = 'timjolson@user.noreplay.github.com',
+    author = 'timjolson',
+    author_email = 'timjolson@user.noreply.github.com',
     packages=find_packages(),
     tests_require=['pytest'],
 )
